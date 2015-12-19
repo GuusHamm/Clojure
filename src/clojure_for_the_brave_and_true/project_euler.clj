5a6348a9a6f1ac0e54e5791d2d46cd4cdca41265
GuusHamm <guushamm@gmail.com>
2015-12-19 15:53:37 +0100

    Made project euler 1

--- src/clojure_for_the_brave_and_true/project_euler.clj
+++ src/clojure_for_the_brave_and_true/project_euler.clj
@@ -14,14 +14,27 @@
 
 (dec9 21)
 
+;Project Euler 1
+
+(defn get_multiples_of_x_y
+  [number max sum multiples_of_x multiples_of_y]
+  (if (> number (dec max))
+    (do (println sum))
+    (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
+          (do (get_multiples_of_x_y (inc number) max (+ sum number) multiples_of_x multiples_of_y))
+          (do (get_multiples_of_x_y (inc number) max sum multiples_of_x multiples_of_y))))))
+
+(get_multiples_of_x_y 1 1000 0 3 5)
+
 ; Project Euler 2
 
 (defn get_fibonacci_number
   [a b total]
   (if (> b 2000000)
     (do (println (str (+ total a b))))
-    (do(if (= (mod (+ a b) 2) 0)
-         (do ( get_fibonacci_number b (+ a b) (+ total (+ a b))) )
-         (do ( get_fibonacci_number b (+ a b) total))))))
+    (do (if (= (mod (+ a b) 2) 0)
+          (do (get_fibonacci_number b (+ a b) (+ total (+ a b))))
+          (do (get_fibonacci_number b (+ a b) total))))))
 
 (get_fibonacci_number 1 2 2)
+
