c364c47b41a7206bba21c8e3423077885dbf26d1
guushamm <guushamm@gmail.com>
2015-12-29 13:55:23 +0100

    Project Euler 4 : Palindrone Product Finished

--- src/clojure_for_the_brave_and_true/project_euler.clj
+++ src/clojure_for_the_brave_and_true/project_euler.clj
@@ -13,37 +13,65 @@
 (def dec9 (dec_maker 9))
 
 (dec9 21)
+;
+;;Project Euler 1
+;(println "Project Euler 1")
+;
+;(defn get_multiples_of_x_y
+;  ([max multiples_of_x multiples_of_y] (get_multiples_of_x_y 1 max 0 multiples_of_x multiples_of_y))
+;  ([number max sum multiples_of_x multiples_of_y]
+;   (if (> number (dec max))
+;     (do (println sum))
+;     (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
+;           (do (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y))
+;           (do (recur (inc number) max sum multiples_of_x multiples_of_y)))))))
+;
+;(defn get_multiples_of_3_5
+;  [max]
+;  (get_multiples_of_x_y max 3 5 ))
+;
+;(get_multiples_of_3_5 1000)
+;
+;; Project Euler 2
+;(println "Project Euler 2")
+;
+;(defn get_fibonacci_number
+;  ([] (get_fibonacci_number 1 2 2 2000000))
+;  ([max] (get_fibonacci_number 1 2 2 max))
+;  ([a b total max]
+;   (if (> b max)
+;     (do (println (str (+ total a b))))
+;     (do (if (= (mod (+ a b) 2) 0)
+;           (do (recur b (+ a b) (+ total (+ a b)) max))
+;           (do (recur b (+ a b) total max)))))))
+;
+;(get_fibonacci_number)
 
-;Project Euler 1
-(println "Project Euler 1")
+; Project Euler 4
+(defn is_palindrone?
+  [number]
+  ; This next bit of cide might seem a bit odd, the reason for this is that reverse
+  ; returns a value in the form of ( d d d ) instead of ddd there for the reverse of
+  ; the reverse to get the original value in the same format
+  (= (reverse (reverse (str number))) (reverse (str number))))
 
-(defn get_multiples_of_x_y
-  ([max multiples_of_x multiples_of_y] (get_multiples_of_x_y 1 max 0 multiples_of_x multiples_of_y))
-  ([number max sum multiples_of_x multiples_of_y]
-   (if (> number (dec max))
-     (do (println sum))
-     (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
-           (do (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y))
-           (do (recur (inc number) max sum multiples_of_x multiples_of_y)))))))
+(defn number_of_length
+  [length]
+  (int (dec (java.lang.Math/pow 10 (int length)))))
 
-(defn get_multiples_of_3_5
-  [max]
-  (get_multiples_of_x_y max 3 5 ))
+(defn palindrone_product
+  ([length] (palindrone_product (number_of_length length) (number_of_length length) 0 length ))
+  ([a b largest length]
+   (if (<= a 0)
+     (str largest)
+     (if (is_palindrone? (* a b))
+       (if (< (int largest) (* a b))
+         (recur a (dec b) (* a b) length)
+         (recur a (dec b) largest length))
+       (if (<= b 0)
+         (recur (dec a) (number_of_length length) largest length)
+         (recur a (dec b) largest length))))))
 
-(get_multiples_of_3_5 1000)
+(println (palindrone_product 3))
 
-; Project Euler 2
-(println "Project Euler 2")
-
-(defn get_fibonacci_number
-  ([] (get_fibonacci_number 1 2 2 2000000))
-  ([max] (get_fibonacci_number 1 2 2 max))
-  ([a b total max]
-   (if (> b max)
-     (do (println (str (+ total a b))))
-     (do (if (= (mod (+ a b) 2) 0)
-           (do (recur b (+ a b) (+ total (+ a b)) max))
-           (do (recur b (+ a b) total max)))))))
-
-(get_fibonacci_number)
 
