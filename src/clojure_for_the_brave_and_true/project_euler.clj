fa3b6d74c64dcd1ea836a18d50935cb56c30a5a0
guushamm <guushamm@gmail.com>
2015-12-29 13:55:48 +0100

    Project Euler 4 : Palindrone Product Finished

--- src/clojure_for_the_brave_and_true/project_euler.clj
+++ src/clojure_for_the_brave_and_true/project_euler.clj
@@ -13,39 +13,39 @@
 (def dec9 (dec_maker 9))
 
 (dec9 21)
-;
-;;Project Euler 1
-;(println "Project Euler 1")
-;
-;(defn get_multiples_of_x_y
-;  ([max multiples_of_x multiples_of_y] (get_multiples_of_x_y 1 max 0 multiples_of_x multiples_of_y))
-;  ([number max sum multiples_of_x multiples_of_y]
-;   (if (> number (dec max))
-;     (do (println sum))
-;     (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
-;           (do (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y))
-;           (do (recur (inc number) max sum multiples_of_x multiples_of_y)))))))
-;
-;(defn get_multiples_of_3_5
-;  [max]
-;  (get_multiples_of_x_y max 3 5 ))
-;
-;(get_multiples_of_3_5 1000)
-;
-;; Project Euler 2
-;(println "Project Euler 2")
-;
-;(defn get_fibonacci_number
-;  ([] (get_fibonacci_number 1 2 2 2000000))
-;  ([max] (get_fibonacci_number 1 2 2 max))
-;  ([a b total max]
-;   (if (> b max)
-;     (do (println (str (+ total a b))))
-;     (do (if (= (mod (+ a b) 2) 0)
-;           (do (recur b (+ a b) (+ total (+ a b)) max))
-;           (do (recur b (+ a b) total max)))))))
-;
-;(get_fibonacci_number)
+
+;Project Euler 1
+(println "Project Euler 1")
+
+(defn get_multiples_of_x_y
+  ([max multiples_of_x multiples_of_y] (get_multiples_of_x_y 1 max 0 multiples_of_x multiples_of_y))
+  ([number max sum multiples_of_x multiples_of_y]
+   (if (> number (dec max))
+     (do (println sum))
+     (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
+           (do (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y))
+           (do (recur (inc number) max sum multiples_of_x multiples_of_y)))))))
+
+(defn get_multiples_of_3_5
+  [max]
+  (get_multiples_of_x_y max 3 5 ))
+
+(get_multiples_of_3_5 1000)
+
+; Project Euler 2
+(println "Project Euler 2")
+
+(defn get_fibonacci_number
+  ([] (get_fibonacci_number 1 2 2 2000000))
+  ([max] (get_fibonacci_number 1 2 2 max))
+  ([a b total max]
+   (if (> b max)
+     (do (println (str (+ total a b))))
+     (do (if (= (mod (+ a b) 2) 0)
+           (do (recur b (+ a b) (+ total (+ a b)) max))
+           (do (recur b (+ a b) total max)))))))
+
+(get_fibonacci_number)
 
 ; Project Euler 4
 (defn is_palindrone?
