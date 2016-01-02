cd9aac991280a1e41f94688709b9acce8552b748
GuusHamm <guushamm@gmai.com>
2016-01-02 20:29:09 +0100

    Update Project Euler 1 & 2, removed some redundant code

--- src/clojure_for_the_brave_and_true/project_euler.clj
+++ src/clojure_for_the_brave_and_true/project_euler.clj
@@ -1,19 +1,5 @@
 (ns clojure-for-the-brave-and-true.project_euler)
 
-; Exercise 2
-(defn number_adder
-  [number]
-  (+ number 100))
-
-; Exercise 3
-(defn dec_maker
-  [dec-by]
-  #(- % dec-by))
-
-(def dec9 (dec_maker 9))
-
-(dec9 21)
-
 ;Project Euler 1
 (println "Project Euler 1")
 
@@ -21,31 +7,30 @@
   ([max multiples_of_x multiples_of_y] (get_multiples_of_x_y 1 max 0 multiples_of_x multiples_of_y))
   ([number max sum multiples_of_x multiples_of_y]
    (if (> number (dec max))
-     (do (println sum))
+     (str sum))
      (do (if (or (= (mod number multiples_of_x) 0) (= (mod number multiples_of_y) 0))
-           (do (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y))
-           (do (recur (inc number) max sum multiples_of_x multiples_of_y)))))))
+           (recur (inc number) max (+ sum number) multiples_of_x multiples_of_y)
+           (recur (inc number) max sum multiples_of_x multiples_of_y))))))
 
 (defn get_multiples_of_3_5
   [max]
   (get_multiples_of_x_y max 3 5 ))
 
-(get_multiples_of_3_5 1000)
+(println (get_multiples_of_3_5 1000))
 
 ; Project Euler 2
 (println "Project Euler 2")
 
 (defn get_fibonacci_number
-  ([] (get_fibonacci_number 1 2 2 2000000))
   ([max] (get_fibonacci_number 1 2 2 max))
   ([a b total max]
    (if (> b max)
-     (do (println (str (+ total a b))))
+     (str (+ total a b))
      (do (if (= (mod (+ a b) 2) 0)
-           (do (recur b (+ a b) (+ total (+ a b)) max))
-           (do (recur b (+ a b) total max)))))))
+           (recur b (+ a b) (+ total (+ a b)) max)
+           (recur b (+ a b) total max))))))
 
-(get_fibonacci_number)
+(println (get_fibonacci_number 2000000))
 
 ; Project Euler 4
 (println "Project Euler 4")
